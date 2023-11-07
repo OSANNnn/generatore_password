a1a0cf020824b56a2617f23013760531ffd6864b
osan <osannnn@gmail.com>
2023-11-08 00:42:51 +0100

    added content to main.c

--- source/main.c
+++ source/main.c
@@ -1,11 +1,34 @@
 //passwordGenerator.c
 //programma che genera password random da 6 a 15 caratteri
 
+#include "proto.h"
 #include <stdio.h>
 
 
 int main()
 {
+    char password[15];
+    int lunghezza;
+    int menu;
 
-    
+    do
+    {
+        printf("[1] Genera password. \n [0] Esci. \n Scegli l'opzione: ");
+        scanf("%d", &menu);
+        if (menu)
+        {
+            if (menu=1)
+            {
+                for (int i = 0; i<lunghezza; i++)
+                {
+                password[i] = randomCharGenerator(i);
+                }    
+            }
+            else
+            {
+                printf("Comando non valido \n\n");
+            }
+        }
+    } while (menu != 0);
+   
 }
--- source/proto.h
+++ source/proto.h
@@ -2,4 +2,5 @@
 //funzione che ritorna un char random
 char randomCharGenerator(int seed);
 
+//funzione per la richiesta e la definizione della lunghezza della password
 int lunghezzaPassword();
